3144f8c8d68f441091ee24f6a052a3c60f751cf6
Flynn <flynnoconnell@gmail.com>
2024-06-11 22:16:04 -0400

    docs: fix conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,35 +13,27 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./'))
 sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../img/'))
 sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
 project = 'caiman-python'
-copyright = '2024, Flynn OConnell'
+copyright = '2024, Elizabeth R. Miller Brain Observatory (MBO) | The Rockefeller University. All Rights Reserved.'
 author = 'Flynn OConnell, Vaziri Lab Members'
-
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.viewcode",
-    'sphinx.ext.napoleon',
-    'sphinx.ext.coverage',
-    'sphinx-prompt',
-    'sphinxcontrib.apidoc',
-    'sphinx_autodoc_typehints',
-    'sphinx.ext.autosummary',
-]
+extensions = ["sphinx.ext.autodoc","sphinxcontrib.images", "sphinxcontrib.video" ,"sphinxcontrib.matlab", "numpydoc", "sphinx.ext.intersphinx", "sphinx.ext.napoleon", "sphinx.ext.autosectionlabel"]
+
+images_config = dict(backend='LightBox2',
+                     default_image_width='100%',
+                     default_show_title='True',
+                     default_group='default'
+    )
 
 pygments_style = 'sphinx'
 templates_path = ['_templates']
@@ -56,18 +48,14 @@ master_doc = 'index'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-
-
 html_theme_options = {
-  "github_url": "https://github.com/ru-rbu",
   "external_links": [
       {"name": "MBO", "url": "https://mbo.rockefeller.edu"},
-      {"name": "Github", "url": "https://www.github.com/ru-rbo"}
-      ],
+  ]
 }
 
 html_theme = 'pydata_sphinx_theme'
-html_title = "Elizabeth R. Miller Brain Observatory Hub"
+html_title = "scanreader"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -77,5 +65,5 @@ html_static_path = ['_static']
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    'rbo.css'
+    'LBM_docs.css'
 ]
