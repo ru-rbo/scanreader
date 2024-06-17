9053f8b1714590b0f31e11fa6dbed929fe3b8afe
Flynn OConnell <flynnoconnell@gmail.com>
2024-06-17 13:07:12 -0400

    hotfix: conf incorrect css/icons

--- docs/conf.py
+++ docs/conf.py
@@ -49,14 +49,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '__pycache__/']
 
 
 html_theme = 'sphinx_book_theme'
+html_logo = 'mbo.ico'
 html_title = "scanreader"
 html_css_files = [
-    'LBM_docs.css'
+    'mbo.css'
 ]
 
 pygments_style = 'sphinx'
 source_suffix = ['.rst', '.md']
-#master_doc = 'index'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -69,4 +69,3 @@ images_config = dict(backend='LightBox2',
                      default_show_title='True',
                      default_group='default')
 
-autodoc_mock_imports = ['numpy', 'tifffile']
